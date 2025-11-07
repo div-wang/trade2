a8e651d4d232cac2708f40e80910e9e6e38086ca
Div <gh110827@gmail.com>
2025-11-07 21:30:27 +0800

    增加pm2进程守护

---
+++ test/index.js
@@ -0,0 +1,25 @@
+const { modifySheetData } = require("../handler/feishu");
+
+const app = async () => {
+  // const {spreadsheet_token, sheet_id} = configs.grabInfo.ya
+  // await modifySheetData(
+  //   spreadsheet_token,
+  //   sheet_id,
+  //   "J24:J24",
+  //   [[398]]
+  // ).then((res) => {
+  //   console.log(res);
+  // });
+
+  // 订单详情
+  // await zhuan.getChildOrderIds("1984923973253595158");
+
+  // 子订单详情
+  // const childOrderInfo = await this.getChildOrderInfo("1984798001071129113", {})
+
+  // 获取imei
+  // const getIMEIResult = await getIMEI("974690130");
+  // console.log("getIMEIResult", getIMEIResult);
+};
+
+module.exports = app;
